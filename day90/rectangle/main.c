920c2df0df80acad0d0db4ab3b880e79744b5a36
Alaeeddin Hajji <69818848+alaeeddinehajji@users.noreply.github.com>
2022-03-10 22:22:52 +0100

    Create main.c

---
+++ day90/rectangle/main.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+int main() {
+    int l=50;
+    int p=58;
+    int s=50;
+    int d=50;
+
+    for (p=1; p<=s; p++)
+    {
+    printf ("\n");
+    for (l=1;l<=d; l++)
+
+    {
+
+    if(p==1 || l==1 || l==d || s==p 
+    || p==s/2 || l==l/2)
+
+    printf("*"); else printf(" ");
+
+
+    }
+  }
+      return 0;
+    }
